ad90f9a74de6af60ae7fa1b91ca7fb5fd6c40cc5
SebastianWTAMU <sgonzalez12@buffs.wtamu.edu>
2024-09-15 21:04:01 -0500

    Ready

--- unit-testing-using-dotnet-test/PrimeService/obj/Debug/net8.0/PrimeService.AssemblyInfo.cs
+++ unit-testing-using-dotnet-test/PrimeService/obj/Debug/net8.0/PrimeService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PrimeService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66bae0aae0c5ea1ec2115772bb2a81d0088bd5a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c13ec4904baba1e944c76cc31855bbf06aade26")]
 [assembly: System.Reflection.AssemblyProductAttribute("PrimeService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PrimeService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
